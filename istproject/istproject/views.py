5c5b6027fff8879abcb580ace2e348cde602bc86
wajeeeehaa <134785721+wajeeeehaa@users.noreply.github.com>
2026-03-11 15:24:37 +0500

    Templates in django

---
+++ istproject/istproject/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hey this is home page of django application")
+    return render (request, "index.html")
+def about(request):
+    return HttpResponse("Hey this is about page of django application")
+
+def contact(request):
+    return HttpResponse("Hey this is contact page of django application")
