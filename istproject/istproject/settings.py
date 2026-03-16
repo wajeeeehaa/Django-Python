9cfe6132b05a90f66a7d5095a9d58ebd3cb80bce
wajeeeehaa <134785721+wajeeeehaa@users.noreply.github.com>
2026-03-16 12:45:54 +0500

    ist html template

--- istproject/istproject/settings.py
+++ istproject/istproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+# it will come to know how to add   static folder 
+STATICFILES_DIRS=[os.path.join(BASE_DIR), 'static']
--- istproject/istproject/views.py
+++ istproject/istproject/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hey this is home page of django application")
-    return render (request, "index.html")
+    return render (request, "website/index.html")
 def about(request):
     return HttpResponse("Hey this is about page of django application")
 
